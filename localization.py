3c3ea0a7715c18e4f940d5803b99566de949c0ef
Jaxon Zeng <zengcb@berkeley.edu>
2023-04-06 18:42:09 -0700

    syncing

--- localization.py
+++ localization.py
@@ -1,17 +1,20 @@
-import torch
+"""
+Python Script for Localization of imagines
 
-# Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5l')
+use YOLOV5 boxing,
+find center of object box and then turn coordinate value into distance relative 
+to angle of gimbal of camera, GPS coordinates of drone and altitude being plugged 
+in as parameters.
 
-# Image
-img = 'https://ultralytics.com/images/zidane.jpg'
+method localization pass in parameters
+camera_angle (due to the front of the drone): the field of the camera (default angle is pointing downward),
+(up/down, left/right, rotating clockwise/counterclockwise)
+height: the height of the drone
+latitude, longtitude: information from GPS
+direction: direction of the drone (angle due north)
 
-# Inference
-results = model(img)
+returns the target latitude, longtitude
+"""
+def localization(camera_angle, height, latitude, longtitude, direction):
 
-results.pandas().xyxy[0]
-#      xmin    ymin    xmax   ymax  confidence  class    name
-# 0  749.50   43.50  1148.0  704.5    0.874023      0  person
-# 1  433.50  433.50   517.5  714.5    0.687988     27     tie
-# 2  114.75  195.75  1095.0  708.0    0.624512      0  person
-# 3  986.00  304.00  1028.0  420.0    0.286865     27     tie
+    return []
