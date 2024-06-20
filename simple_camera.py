2a768f1a616547831f51e116c72cfcd1a60a39bf
John Lomax <jclomax25@berkeley.edu>
2024-06-19 22:53:39 -0700

    Adjusted to incorporate YOLO

--- simple_camera.py
+++ simple_camera.py
@@ -7,7 +7,7 @@
 
 import cv2
 
-#from ultralytics import YOLO
+from ultralytics import YOLO
 
 """ 
 gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
@@ -15,6 +15,91 @@ Flip the image by setting the flip_method (most common values: 0 and 2)
 display_width and display_height determine the size of each camera pane in the window on the screen
 Default 1920x1080 displayd in a 1/4 size window
 """
+
+model = YOLO("yolov8n.pt")
+
+classNames = [
+    "person",
+    "bicycle",
+    "car",
+    "motorbike",
+    "aeroplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "backpack",
+    "umbrella",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "sofa",
+    "pottedplant",
+    "bed",
+    "diningtable",
+    "toilet",
+    "tvmonitor",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+]
 
 def gstreamer_pipeline(
     sensor_id=0,
@@ -54,18 +139,44 @@ def show_camera():
     if video_capture.isOpened():
         try:
             window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
-            while True:
+            while cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
                 ret_val, frame = video_capture.read()
+                results = model(frame, stream=True)
+                for r in results:
+                    boxes = r.boxes
+
+                    for box in boxes:
+                        # bounding box
+                        x1, y1, x2, y2 = box.xyxy[0]
+                        x1, y1, x2, y2 = (
+                            int(x1),
+                            int(y1),
+                            int(x2),
+                            int(y2),
+                        )  # convert to int values
+
+                        # put box in cam
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                        # class name
+                        cls = int(box.cls[0])
+                        print("Class name -->", classNames[cls])
+
+                        # object details
+                        org = [x1, y1]
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        fontScale = 1
+                        color = (255, 0, 0)
+                        thickness = 2
+
+                        cv2.putText(
+                            frame, classNames[cls], org, font, fontScale, color, thickness
+                        )
                 # Check to see if the user closed the window
                 # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
                 # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
-                if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
-                    cv2.imshow(window_title, frame)
-                    #results = model(frame)
-                    #print(results)
-                else:
-                    break 
-                keyCode = cv2.waitKey(10) & 0xFF
+                cv2.imshow(window_title, frame)
+                 
+                keyCode = cv2.waitKey(30) & 0xFF
                 # Stop the program on the ESC key or 'q'
                 if keyCode == 27 or keyCode == ord('q'):
                     break
@@ -75,7 +186,5 @@ def show_camera():
     else:
         print("Error: Unable to open camera")
 
-
 if __name__ == "__main__":
-    #model = YOLO("yolov8n.pt")
     show_camera()
