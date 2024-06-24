8eb706f94fc83ea213bc4ed7760a14bbcc553070
John Lomax <jclomax25@berkeley.edu>
2024-06-24 18:11:37 -0400

    added

--- mission_basic.py
+++ mission_basic.py
@@ -17,6 +17,9 @@ Full documentation is provided at https://dronekit-python.readthedocs.io/en/late
 import cv2
 
 import time
+import math
+
+import numpy as np
 
 from ultralytics import YOLO
 from pymavlink import mavutil
@@ -42,7 +45,7 @@ airfield_MSL = 43.2816
 
 last_searched = 11
 
-lastwaypoint = 45
+lastwaypoint = 1
 
 drop_count = 0
 
@@ -100,6 +103,34 @@ window_title = "CSI Camera"
 
 print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
+
+def get_distance_metres(aLocation1, aLocation2):
+    """
+    Returns the ground distance in metres between two LocationGlobal objects.
+
+    This method is an approximation, and will not be accurate over large distances and close to the 
+    earth's poles. It comes from the ArduPilot test code: 
+    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+    """
+    dlat = aLocation2.lat - aLocation1.lat
+    dlong = aLocation2.lon - aLocation1.lon
+    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+def distance_to_current_waypoint():
+    """
+    Gets distance in metres to the current waypoint. 
+    It returns None for the first waypoint (Home location).
+    """
+    nextwaypoint = vehicle.commands.next
+    if nextwaypoint==0:
+        return None
+    missionitem=vehicle.commands[nextwaypoint-1] #commands are zero indexed
+    lat = missionitem.x
+    lon = missionitem.y
+    alt = missionitem.z
+    targetWaypointLocation = LocationGlobalRelative(lat,lon,alt)
+    distancetopoint = get_distance_metres(vehicle.location.global_frame, targetWaypointLocation)
+    return distancetopoint
 
 def adds_wypt_mission():
     """
@@ -181,7 +212,7 @@ vehicle.mode = VehicleMode("AUTO")
 #   distance to the next waypoint.
 while True:
     nextwaypoint=vehicle.commands.next
-    if nextwaypoint>10:
+    if nextwaypoint<10:
         camSet = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM),width=3840,height=2160,framerate=29/1,format=NV12 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,width=1920,height=1080,format=BGR ! queue ! appsink'
         video_capture = cv2.VideoCapture(camSet, cv2.CAP_GSTREAMER)
         if video_capture.isOpened():
@@ -205,8 +236,13 @@ while True:
                             # put box in cam
                             cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
                             # class name
+                            roi = frame[y1:y2, x1:x2]
+                            roi = cv2.resize(roi, (300, 300))
+                            
+                            #class name
                             cls = int(box.cls[0])
-                            print("Class name -->", classNames[cls])
+                            print("Class name -->", model.names[cls])
+                            cv2.imwrite("Image_"+ model.names[cls] + ".jpg", roi)
 
                             # object details
                             org = [x1, y1]
@@ -239,3 +275,12 @@ while True:
     if nextwaypoint == lastwaypoint:
         break
     time.sleep(1)
+    
+print('Return to launch')
+vehicle.mode = VehicleMode("RTL")
+video_capture.release()
+
+
+#Close vehicle object before exiting script
+print("Close vehicle object")
+vehicle.close()
