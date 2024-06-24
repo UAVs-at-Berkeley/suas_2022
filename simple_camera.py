78dde7bea99f38bfa8fe5862e2d00bc0e362855a
John Cole Lomax <jclomax25@berkeley.edu>
2024-06-24 16:53:38 -0400

    test

--- simple_camera.py
+++ simple_camera.py
@@ -128,63 +128,59 @@ def gstreamer_pipeline(
         )
     )
 
+window_title = "CSI Camera"
 
-def show_camera():
-    window_title = "CSI Camera"
+# To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+print(gstreamer_pipeline(flip_method=0))
+camSet = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM),width=3840,height=2160,framerate=29/1,format=NV12 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,width=1920,height=1080,format=BGR ! queue ! appsink'
+video_capture = cv2.VideoCapture(camSet, cv2.CAP_GSTREAMER)
+if video_capture.isOpened():
+    try:
+        window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
+        while cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
+            ret_val, frame = video_capture.read()
+            results = model(frame, stream=True)
+            for r in results:
+                boxes = r.boxes
 
-    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-    print(gstreamer_pipeline(flip_method=0))
-    camSet = 'nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM),width=3840,height=2160,framerate=29/1,format=NV12 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,width=1920,height=1080,format=BGR ! queue ! appsink'
-    video_capture = cv2.VideoCapture(camSet, cv2.CAP_GSTREAMER)
-    if video_capture.isOpened():
-        try:
-            window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
-            while cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
-                ret_val, frame = video_capture.read()
-                results = model(frame, stream=True)
-                for r in results:
-                    boxes = r.boxes
+                for box in boxes:
+                    # bounding box
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    x1, y1, x2, y2 = (
+                        int(x1),
+                        int(y1),
+                        int(x2),
+                        int(y2),
+                    )  # convert to int values
 
-                    for box in boxes:
-                        # bounding box
-                        x1, y1, x2, y2 = box.xyxy[0]
-                        x1, y1, x2, y2 = (
-                            int(x1),
-                            int(y1),
-                            int(x2),
-                            int(y2),
-                        )  # convert to int values
+                    # put box in cam
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                    # class name
+                    cls = int(box.cls[0])
+                    print("Class name -->", classNames[cls])
 
-                        # put box in cam
-                        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
-                        # class name
-                        cls = int(box.cls[0])
-                        print("Class name -->", classNames[cls])
+                    # object details
+                    org = [x1, y1]
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    fontScale = 1
+                    color = (255, 0, 0)
+                    thickness = 2
 
-                        # object details
-                        org = [x1, y1]
-                        font = cv2.FONT_HERSHEY_SIMPLEX
-                        fontScale = 1
-                        color = (255, 0, 0)
-                        thickness = 2
+                    cv2.putText(
+                        frame, classNames[cls], org, font, fontScale, color, thickness
+                    )
+            # Check to see if the user closed the window
+            # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
+            # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
+            cv2.imshow(window_title, frame)
+                
+            keyCode = cv2.waitKey(30) & 0xFF
+            # Stop the program on the ESC key or 'q'
+            if keyCode == 27 or keyCode == ord('q'):
+                break
+    finally:
+        video_capture.release()
+        cv2.destroyAllWindows()
+else:
+    print("Error: Unable to open camera")
 
-                        cv2.putText(
-                            frame, classNames[cls], org, font, fontScale, color, thickness
-                        )
-                # Check to see if the user closed the window
-                # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
-                # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
-                cv2.imshow(window_title, frame)
-                 
-                keyCode = cv2.waitKey(30) & 0xFF
-                # Stop the program on the ESC key or 'q'
-                if keyCode == 27 or keyCode == ord('q'):
-                    break
-        finally:
-            video_capture.release()
-            cv2.destroyAllWindows()
-    else:
-        print("Error: Unable to open camera")
-
-if __name__ == "__main__":
-    show_camera()
