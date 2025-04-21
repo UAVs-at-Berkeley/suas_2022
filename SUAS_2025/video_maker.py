06106f7474f721524111b73b31bb9c85f02b9796
John Cole Lomax <jclomax25@berkeley.edu>
2025-04-20 23:20:41 -0700

    multithread

--- SUAS_2025/video_maker.py
+++ SUAS_2025/video_maker.py
@@ -1,56 +1,48 @@
 from time import sleep
-from multiprocessing import Process
 from threading import Thread
 import cv2
 import os
 
 PATH_OF_SCRIPT = os.path.dirname(os.path.abspath(__file__)) #local directory, NOT the working directory
 RSTP_URL = "rtsp://192.168.144.25:8554/main.264"
-FOURCC = cv2.VideoWriter_fourcc(*'H264')  
+FOURCC = cv2.VideoWriter_fourcc(*'H264')
 
 class VideoMaker:
     '''
     A class that creates mp4 videos via a videocapture object \n
     Constructors: \n
-        VideoCapture capture: a opencv videocaputre object 
+        VideoCapture stream: a opencv videocaputre object 
         str parallelism(default = "threading"): a string corresponding to the type of parallelism to use, if a string other than "multiprocessing" is passed, uses Threading
     '''
-    def __init__(self, capture, parallelism = "threading"):
+    def __init__(self, stream):
         self.recording_flag = True
-        self.parallelism = parallelism
-        self.capture = capture
-        self.parallel_process = None
+        self.stream = stream
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
 
-        frame_width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = self.capture.get(cv2.CAP_PROP_FPS)
+        frame_width = int(self.stream.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = self.stream.get(cv2.CAP_PROP_FPS)
+        fourcc2 = self.stream.get(CAP_PROP_FOURCC)
 
         i = 0
         while os.path.exists(f'{PATH_OF_SCRIPT}/video_{i}.avi'):
             i += 1
 
-        self.data = cv2.VideoWriter(f'{PATH_OF_SCRIPT}/video_{i}.avi', FOURCC, fps, (frame_width, frame_height))
+        self.data = cv2.VideoWriter(f'{PATH_OF_SCRIPT}/video_{i}.avi', cv2.VideoWriter_fourcc(*'MJPG'), fps, (frame_width, frame_height))
 
     def recording_process(self):
         while self.recording_flag:
-            ret, frame = self.capture.read()
-            self.data.write(frame)
+            (self.grabbed, self.frame) = self.stream.read()
+            if self.grabbed:
+                self.data.write(self.frame)
 
     def start(self):
-        if self.parallel_process != None:
-            raise Exception("Cannot start video recording when it has already started")
-        if self.parallelism == "multiprocessing":
-            writing_loop = Process(target= self.recording_process)
-        else: 
-            writing_loop = Thread(target= self.recording_process)
-
-        self.parallel_process = writing_loop
-        writing_loop.start()
-
+        Thread(target=self.recording_process, args=()).start()
+        return self
+        
     def stop(self):
         self.recording_flag = False
-        if self.parallel_process is not None:
-            self.parallel_process.join()
         self.data.release()
 
 def basic_sanity_test():
@@ -60,7 +52,7 @@ def basic_sanity_test():
     capture = cv2.VideoCapture(RSTP_URL)
     video_maker = VideoMaker(capture)
     video_maker.start()
-    sleep(180)
+    time.sleep(180)
     video_maker.stop()
     capture.release()
 
